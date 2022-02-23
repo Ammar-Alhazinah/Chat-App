ada28f54a875db35015b99720cb06875b5e272f7
Ammar-Alhazinah <ammar.alhazinah@gmail.com>
2022-02-23 14:23:25 +0200

    Add menu items and drawer header

--- app/src/main/java/edu/cs/birzeit/a1172482/chatapp/MainActivity.java
+++ app/src/main/java/edu/cs/birzeit/a1172482/chatapp/MainActivity.java
@@ -1,14 +1,35 @@
 package edu.cs.birzeit.a1172482.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Chat App");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView=findViewById(R.id.navView);
+
+        View view = navigationView.inflateHeaderView(R.layout.drawer_header);
+
     }
 }
