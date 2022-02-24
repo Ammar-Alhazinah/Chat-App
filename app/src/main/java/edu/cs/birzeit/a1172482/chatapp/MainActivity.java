20cbd42e0a1dfe12cfe229dc96b21fcf76cd5aa4
Ammar-Alhazinah <ammar.alhazinah@gmail.com>
2022-02-24 10:59:30 +0200

    Click listener on menu items and put logout in another group

--- app/src/main/java/edu/cs/birzeit/a1172482/chatapp/MainActivity.java
+++ app/src/main/java/edu/cs/birzeit/a1172482/chatapp/MainActivity.java
@@ -1,15 +1,20 @@
 package edu.cs.birzeit.a1172482.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     Toolbar toolbar;
     DrawerLayout drawerLayout;
@@ -27,9 +32,46 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
 
         drawerLayout = findViewById(R.id.drawerLayout);
-        navigationView=findViewById(R.id.navView);
+        navigationView = findViewById(R.id.navView);
 
         View view = navigationView.inflateHeaderView(R.layout.drawer_header);
 
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.profile:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.friend:
+                Toast.makeText(this, "Friend", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.findFriends:
+                Toast.makeText(this, "Find Friends", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.chat:
+                Toast.makeText(this, "Chat", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()== android.R.id.home){
+            drawerLayout.openDrawer(GravityCompat.START);
+            return  true;
+        }
+        return true;
     }
 }
