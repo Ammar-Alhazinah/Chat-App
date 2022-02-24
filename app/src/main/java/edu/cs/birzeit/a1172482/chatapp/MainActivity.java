29d3977a00cb382681f7a2e93d3dc6faec2f33e5
Ammar-Alhazinah <ammar.alhazinah@gmail.com>
2022-02-24 11:24:09 +0200

    Read profile image and username from database

--- app/src/main/java/edu/cs/birzeit/a1172482/chatapp/MainActivity.java
+++ app/src/main/java/edu/cs/birzeit/a1172482/chatapp/MainActivity.java
@@ -6,19 +6,39 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    DatabaseReference mUserRef;
+
+    String profileImageUrlV, usernameV;
+    CircleImageView profileImageHeader;
+    TextView usernameHeader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +51,51 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
 
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mUserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+
         drawerLayout = findViewById(R.id.drawerLayout);
         navigationView = findViewById(R.id.navView);
 
         View view = navigationView.inflateHeaderView(R.layout.drawer_header);
+        profileImageHeader=view.findViewById(R.id.profileImage_header);
+        usernameHeader=view.findViewById(R.id.username_header);
 
         navigationView.setNavigationItemSelectedListener(this);
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mUser == null){
+            SendUserToLoginActivity();
+        } else {
+            mUserRef.child(mUser.getUid()).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()){
+                        profileImageUrlV=snapshot.child("profileImage").getValue().toString();
+                        usernameV=snapshot.child("username").getValue().toString();
+                        Picasso.get().load(profileImageUrlV).into(profileImageHeader);
+                        usernameHeader.setText(usernameV);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(MainActivity.this, "Sorry!, something going wrong", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     @Override
